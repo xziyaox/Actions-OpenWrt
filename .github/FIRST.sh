f1032a68274ec3713c139ef3089a29dd113b5b22
xziyaox <141825184+xziyaox@users.noreply.github.com>
2025-03-09 14:34:46 +0800

    Update FIRST.sh

--- .github/FIRST.sh
+++ .github/FIRST.sh
@@ -9,4 +9,4 @@
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
